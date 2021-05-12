436c3f3a58ae04bb0b848508bd127b565526c5ee
Olga Kupryjaniuk <olga.kupryjaniuk@gmail.com>
2021-05-12 19:37:58 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,93 @@
+{
+    const tasks = [
+        {
+            content: "odrobić lekcje",
+            done: false,
+        },
+        {
+            content: "zjeść kolację",
+            done: true,
+        },
+    ];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => { 
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, index) => {
+        removeButton.addEventListener("click", () => {
+            removeTask(index);
+        });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+        toggleDoneButton.addEventListener("click", () => {
+            toggleTaskDone(index);
+        });
+    });
+
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="taskList__item">
+            <button class="taskList__button taskList__button--done js-done">
+            ${task.done ? "&#10003;" : ""}</button>
+
+            <span class="${task.done ? " tasksList__spanTask--done" : ""}">
+            ${task.content}</span>
+
+            <button class="taskList__button taskList__button--remove js-remove">&#128465;</button>
+            </li>
+            `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+       bindEvents();
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent)
+    };
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+    init();
+}
